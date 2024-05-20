6f8d10d0828899d24e1f1fe72dc0e5e4b1e1ee46
Karleia Coleho <karleia.c.o.s@gamil.com>
2024-05-20 12:09:36 -0300

    Se tiver saldo, modificar e mostrar saldo atual. Se não, mostrar saldo

--- src/main/java/dio/CaixaEletronico.java
+++ src/main/java/dio/CaixaEletronico.java
@@ -1,8 +1,15 @@
 package dio;
 
-public class Main {
+public class CaixaEletronico {
     public static void main(String[] args) {
 
-        
+        //Controle de fluxo simples
+        double saldo = 25.0;
+        double valorSolicitado = 17.0;
+
+        if (valorSolicitado < saldo)
+            saldo -= valorSolicitado;
+
+         System.out.println(saldo);
     }
 }

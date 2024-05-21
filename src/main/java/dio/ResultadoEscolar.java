c35fa1e4e15f8bc999373a2e9ceda190fd22dd4e
Karleia Coleho <karleia.c.o.s@gamil.com>
2024-05-21 10:42:16 -0300

    Reefatoração para demonstrar aplicação de mais recursos usando condicional ternária

--- src/main/java/dio/ResultadoEscolar.java
+++ src/main/java/dio/ResultadoEscolar.java
@@ -3,9 +3,9 @@ package dio;
 public class ResultadoEscolar {
     public static void main(String[] args) {
         // condicional ternaria.
-        int nota = 10;
+        int nota = 5;
 
-        String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+        String resultado = nota >= 7 ? "Aprovado" : nota >= 5? "Recuperação" : "Reprovado";
 
         System.out.println(resultado);
     }

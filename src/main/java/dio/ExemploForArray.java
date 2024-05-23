ffa8cc8a84e1e1b351041495cc96f0b5d2185eca
Karleia Coleho <karleia.c.o.s@gamil.com>
2024-05-23 14:56:35 -0300

    Refatorando o código com uso do foreach

--- src/main/java/dio/ExemploForArray.java
+++ src/main/java/dio/ExemploForArray.java
@@ -6,9 +6,15 @@ public class ExemploForArray {
 
         //Construindo um exemplo para imprimir o nome do aluno que está contido em cada ídice do Array
         String alunos [] = {"FELIPE" , "JONAS" , "JULIA" , "MARCOS"};
-
+    /*
         for(int i = 0 ; i < alunos.length ; i++) {
             System.out.println("O aluno indicado no indice i=" + i + " é " + alunos[i]);
+        }
+    */
+
+        //Refatorando com aplicação do foreach
+        for (String aluno : alunos) {
+            System.out.println("O nome do aluno é: " + aluno);
         }
     }
 }

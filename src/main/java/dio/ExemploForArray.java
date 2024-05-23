516d13bcfbe91eed37b158edd7a068f78c41de7c
Karleia Coleho <karleia.c.o.s@gamil.com>
2024-05-23 14:49:29 -0300

    Exepemplificando aplicação for com Array

---
+++ src/main/java/dio/ExemploForArray.java
@@ -0,0 +1,14 @@
+package dio;
+
+public class ExemploForArray {
+
+    public static void main(String[] args) {
+
+        //Construindo um exemplo para imprimir o nome do aluno que está contido em cada ídice do Array
+        String alunos [] = {"FELIPE" , "JONAS" , "JULIA" , "MARCOS"};
+
+        for(int i = 0 ; i < alunos.length ; i++) {
+            System.out.println("O aluno indicado no indice i=" + i + " é " + alunos[i]);
+        }
+    }
+}
